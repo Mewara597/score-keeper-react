f961b84350988169e5dc893ec841c2dbf3e0af46
SIDDARTH <83221360+Mewara597@users.noreply.github.com>
2023-05-30 23:41:35 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const reactHeading = React.CreateElement("h1", {
-  className: "head",
-  id: "reactHead",
-  children: "Hello React",
-});
-
-ReactDom.creatRoot(document.getElementById("root")).render(reactHeading);
-
-console.log("react elem", reactHeading);
